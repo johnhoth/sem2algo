4aa542c39e51c8fe9ce33e9a1c425254d1d33cbe
johnhoth <zharov.ia@phystech.edu>
2021-05-21 23:31:36 +0300

    moved solution to it's own function

--- 2-6-8.cpp
+++ 2-6-8.cpp
@@ -7,16 +7,8 @@ using namespace std;
 
 const int INF = 1e5 + 7;
 
-int main() {
-	int n, m;
-	cin >> n >> m;
-	vector <int> a(n + 1), b(m + 1);
-	for (int i = 0; i < n; ++i) {
-		cin >> a[i + 1];
-	}
-	for (int i = 0; i < m; ++i) {
-		cin >> b[i + 1];
-	}
+
+int length_of_longest_common_increasing_subsequence(vector<int> &a, vector<int> &b) {
 	vector <vector <int>> d(n + 1, vector <int> (m + 1, 0));
 	for (int i = 1; i <= n; ++i) {
 		int ind = 0, best = 0;
@@ -33,19 +25,30 @@ int main() {
 		}
 	}
 
-	// for (int i=0; i<n; ++i) {
-	// 	for (int j=0; j<m; ++j) {
-	// 		cout <<d[i][j] << ' ';
-	// 	}
-	// 	cout << endl;
-	// }
-
 	int res = 0;
 	for (int j = 1; j <= m; ++j) {
 		if (d[n][j] > d[n][res]) {
 			res = j;
 		}
 	}
-	cout << d[n][res] << endl;
+	
+	return d[n][res];
+}
+
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector <int> a(n + 1), b(m + 1);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i + 1];
+	}
+	for (int i = 0; i < m; ++i) {
+		cin >> b[i + 1];
+	}
+
+	
+	cout << length_of_longest_common_increasing_subsequence(a, b) << endl;
+
 	return 0;
 }
