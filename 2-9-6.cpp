612aa243d6ac9ae99a2cf5c3bc48fee6d4024391
johnhoth <zharov.ia@phystech.edu>
2021-05-22 18:46:21 +0300

    moved work with console to main and moved solution to propper function

--- 2-9-6.cpp
+++ 2-9-6.cpp
@@ -7,7 +7,35 @@ using namespace std;
 
 const int INF = 1e9+7;
 
-void solve() {
+int weight_of_minimal_path_between(const vector<vector<pair<int, int>>> &g, int s, int t) {
+    vector<int> dist(n, INF); // dist[i] - distance from s to i
+
+    set<pair<int, int>> q; // queue of unrelaxed verticies
+
+    dist[s] = 0;
+    q.insert(make_pair(0, s));
+    while (!q.empty()) {
+        int v = q.begin()->second;
+        q.erase(q.begin());
+        for (auto to : g[v]) {
+            int len = to.second;
+            int u = to.first;
+            if (dist[u] > dist[v] + len) {
+                dist[u] = dist[v] + len;
+                q.insert(make_pair(dist[u], u));
+            }
+        }
+    }
+
+    if (dist[t] == INF) {
+        return -1; // no way :(
+    } else {
+        return dist[t];
+    }
+}
+
+
+int main() {
     int n, m;
     cin >> n >> m;
     vector<vector<pair<int, int>>> g(n);
@@ -25,34 +53,7 @@ void solve() {
         g[x].push_back({y, c});
         g[y].push_back({x, c});
     }
-
-    vector<int> dist(n, INF);
-
-    set<pair<int, int>> q;
-
-    dist[s] = 0;
-    q.insert(make_pair(0, s));
-    while (!q.empty()) {
-        int v = q.begin()->second;
-        q.erase(q.begin());
-        for (auto to : g[v]) {
-            int len = to.second;
-            int u = to.first;
-            if (dist[u] > dist[v] + len) {
-                dist[u] = dist[v] + len;
-                q.insert(make_pair(dist[u], u));
-            }
-        }
-    }
-    if (dist[t] == INF) {
-        cout << -1 << endl;
-    } else {
-        cout << dist[t] << endl;
-    }
+    
+    cout << weight_of_minimal_path_between(s, t) << endl;
 }
 
-
-int main() {
-    solve();
-    return 0;
-}
